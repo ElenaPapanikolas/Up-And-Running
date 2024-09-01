75e421d2c71fc3f1bafcd8b6b62a52e37e14837d
Elena Papanikolas <epapanikolas@hotmail.com>
2024-08-31 18:59:19 -0600

    Removed default React code, imported Chakra UI, added background image

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// Imports `ChakraProvider` component
+import { ChakraProvider } from '@chakra-ui/react'
+// Imports the extendTheme function
+import { extendTheme } from '@chakra-ui/react'
 import App from './App.jsx'
 import './index.css'
 
+const theme = extendTheme({
+  styles: {
+    global: {
+      'html, body': {
+        //background image
+        backgroundImage: 'url("./images/technology.jpg")',
+        backgroundColor: 'black' ,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundAttachment: 'fixed',
+        height: '100%',
+        margin: 0,
+
+        // Font styles
+        // fontFamily: '',
+        // fontSize: '18px',
+      },
+    },
+  },
+});
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ChakraProvider theme={theme}>
     <App />
+    </ChakraProvider>
   </StrictMode>,
 )
