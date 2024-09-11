02369b0e3b13be86106783f3db95dc752405df28
Elena Papanikolas <epapanikolas@hotmail.com>
2024-09-10 18:29:29 -0600

    Updated About and Project components

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { Box, Image, Text, Stack, IconButton } from '@chakra-ui/react';
+import { Box, Image, Text, Stack, IconButton, Tooltip } from '@chakra-ui/react';
 import { FaGithub } from 'react-icons/fa';
 import { PiBrowsersBold } from "react-icons/pi";
 import { motion } from 'framer-motion';
@@ -10,40 +10,47 @@ export default function Project({ title, description, image, github, deployed })
     const styles= {
         Thumbnail: {
             height: '350px',
+            width: '350px',
         },
-    }
+    };
    
     return (
         <Box borderWidth='1px' borderRadius='md' overflow='hidden' p={4} boxShadow='md' borderColor='black'>
-            <Image src={image} alt={title}  objectFit='cover' style={styles.Thumbnail}/>
+            <Box display='flex' justifyContent='center'>
+                <Image src={image} alt={title}  objectFit='cover' style={styles.Thumbnail} borderRadius='md' />
+            </Box>
             <Stack spacing={3} mt={4}>
                 <Text fontSize='xl' fontWeight='bold'>{title}</Text>
                 <Text>{description}</Text>
             </Stack>
             <Stack spacing={2} align='center'>
                 <Stack direction='row' spacing={2}> 
-                    <motion.div whileHover={{ scale: 1.3 }}>
-                        <IconButton 
-                            as='a'
-                            href={github}
-                            aria-label='Link to GitHub repository'
-                            icon={<FaGithub />}
-                            colorScheme='yellow'
-                            target='_blank'
-                            rel='noopener noreferrer'
-                        />
-                    </motion.div>
-                    <motion.div whileHover={{ scale: 1.3 }}>
-                        <IconButton 
-                            as='a'
-                            href={deployed}
-                            aria-label='Link to deployed application'
-                            icon={<PiBrowsersBold />}
-                            colorScheme='yellow'
-                            target='_blank'
-                            rel='noopener noreferrer'
-                        />
-                    </motion.div>
+                    <Tooltip label='GitHub Repository' aria-label='GitHub Repository Tooltip'>
+                        <motion.div whileHover={{ scale: 1.3 }}>
+                            <IconButton 
+                                as='a'
+                                href={github}
+                                aria-label='Link to GitHub repository'
+                                icon={<FaGithub />}
+                                colorScheme='yellow'
+                                target='_blank'
+                                rel='noopener noreferrer'
+                            />
+                        </motion.div>
+                    </Tooltip>
+                    <Tooltip label='Deployed Application' aria-label='Deployed Application Tooltip'>
+                        <motion.div whileHover={{ scale: 1.3 }}>
+                            <IconButton 
+                                as='a'
+                                href={deployed}
+                                aria-label='Link to deployed application'
+                                icon={<PiBrowsersBold />}
+                                colorScheme='yellow'
+                                target='_blank'
+                                rel='noopener noreferrer'
+                            />
+                        </motion.div>
+                    </Tooltip>
                 </Stack>
             </Stack>
         </Box>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -14,21 +14,29 @@ export default function About() {
     return (
         <div>
 
-            <Box display='flex' alignItems='center' boxSize='300px'>
-                <Image src='./src/assets/images/photo1.jpeg' alt='Photo of Elena Papanikolas' style={styles.Image} mx={3} />
-            </Box>  
-        
-            <Container>
-                <Box>
-                    <Heading style= {styles.Heading}>
-                        About Me
-                    </Heading>
+            <Box>
+                <Box display='flex' alignItems='center' boxSize='300px'>
+                    <Image src='./src/assets/images/photo1.jpeg' alt='Photo of Elena Papanikolas' style={styles.Image} mx={4}  />
+                </Box>  
+            
+                <Container>
+                    <Box m={7} p={4} borderWidth={1} borderRadius='md' boxShadow='md' borderColor='black'>
+                        <Heading style= {styles.Heading} display='flex' justifyContent='center'>
+                            About Me
+                        </Heading>
 
-                    <Text mx={3} mt={4}>
-                        Hey there! Im Elena, a passionate junior developer specializing in JavaScript, and eager to continue learning and expanding my skill set.
-                    </Text>
-                </Box>    
-            </Container>
+                        <Text mx={3} mt={4}>
+                            Hey there! Im Elena, a passionate junior developer specializing in JavaScript, and eager to continue learning and expanding my skill set.
+                        </Text>
+                        <Text mx={3} mt={4}>
+                            I&apos;m based out of Salt Lake City.
+                        </Text>
+                        <Text mx={3} mt={4}>
+                            Welcome to my portfolio using React and Chakra UI. Thanks for being here!
+                        </Text>
+                    </Box>    
+                </Container>
+            </Box>
 
         </div>
         
